da3aa601020cd9915750d519cbd5ceb9ded877bb
Shiwang1-hi <shiwangverma660@gmail.com>
2025-10-25 20:57:46 +0530

    Fix: folder casing for CartContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
